cfa46e536d3f3d721fbe88eca20af88582ed2cb9
ostojicfilip <85172397+ostojicfilip@users.noreply.github.com>
2022-06-19 19:25:33 +0200

    Update pouzdani_intervali.py

--- pouzdani_intervali.py
+++ pouzdani_intervali.py
@@ -25,54 +25,15 @@ s=1090.9510720299668
 interval2=scipy.stats.t.interval(alpha=0.85, df=n-1, loc=x, scale=s) 
 
 #testiranje hipoteza
-#Zadatak 1.
-#podaci:
-mu=1
-sigma=0.03
-n=12
-x=0.97
-s=0.04
-alpha=0.05
-
-#test varijance:
-#kriticna vrijednost:
-critical_chi = chi2.ppf(1-alpha, n-1)
-#vrijednost:
-chi_exp=(n-1)*((s*s)/(sigma*sigma))
-if chi_exp < critical_chi:
-	print('Prihvaćamo hipotezu H0')
-	a=1
-else:
-	print('Ne prihvaćamo hipotezu H0')
-	a=0
-
-#test ocekivanja:
-#kriticna vrijednost:
-critical_t= t.ppf(1-alpha,n-1)
-#vrijednost:
-t_exp=(x-mu)/(s/math.sqrt(n))
-if abs(t_exp) < critical_t:
-	print('Prihvaćamo hipotezu H0')
-	b=1
-else:
-	print('Ne prihvaćamo hipotezu H0')
-	b=0
-
-#Jesu li rezultati mjerenja u skladu s deklaracijom?
-if a==1 and b==1:
-	print('Rezultati mjerenja su u skladu s deklaracijom!')
-else:
-	print('Rezultati mjerenja nisu u skladu s deklaracijom!')
-
-#Zadatak 3. 
+#Zadatak 1. 
 
 #podaci:
-n1=8
-x1=12.56
-s1=1.36
-n2=11
-x2=13.01
-s2=0.84
+n1=277
+x1=235.22743682310468
+s1=1090.9510720299668
+n2=200
+x2=#cekam podatke
+s2=#cekam podatke
 alpha=0.05
 
 #test varijance:
@@ -102,3 +63,5 @@ if a==1 and b==1:
 	print('Veličine su slične, ne razlikuju se bitno!')
 else:
 	print('Veličine nisu slične; bitno se razlikuju!')
+	
+#Zadatak 2.
